3864da6d01ef6f6addc3642815215734b99f7175
Mobin Ghasempoor <ghoghnoose.dana@gmail.com>
2024-06-18 15:31:29 +0330

    feat: Feat add role dropdown to admin config page

--- backend/views/settings.php
+++ backend/views/settings.php
@@ -5,6 +5,7 @@ use EZPZ_TWEAKS\Engine\Settings\Settings;
 
 (new Register)->init();
 $page = 'wpezpz-tweaks';
+
 ?>
 <?php require_once( EZPZ_TWEAKS_PLUGIN_ROOT . 'backend/views/header.php' ); ?>
 <div class="ezpz-full-w ezpz-header-breadcrumb">
@@ -23,11 +24,17 @@ $page = 'wpezpz-tweaks';
 	<!-- Start tabs -->
 	<?php
 	Render::navigation( $page );
+	$form   = 'wpezpz-tweaks_options_'.(empty($_GET['tab'])?'customizing-branding':$_GET['tab']);
+    
 	?>
 
 	<!-- End tabs -->
 
 </div>
+<p style="position:relative;top:24px;margin: 0 8px;">
+<?php echo ezpz_option_dropdown();//functions.php ?>
+<input type="hidden" name="ezpz_nonce" form="<?php echo $form; ?>" value="<?php echo wp_create_nonce('Acti0n_Save_0pti0n'); ?>"/>
+</p>
 <div class="ezpz-tweaks-tabs ezpz-tweaks-tabs-content">
 	<?php 	Render::tabs( $page ); ?>
 </div>
